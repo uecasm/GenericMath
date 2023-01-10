239fed2ed007b35fd8271d3db0dab1a4895f3501
Gavin Lambert <uecasm@users.noreply.github.com>
2023-01-10 15:10:34 +1300

    Generic math working in .NET 6

---
+++ Library/MathUtilities.cs
@@ -0,0 +1,9 @@
+﻿namespace Library;
+
+public static class MathUtilities
+{
+    public static T Add<T>(T a, T b) where T : struct, INumber<T>
+    {
+        return a + b;
+    }
+}
---
+++ Net6/Program.cs
@@ -0,0 +1,7 @@
+﻿using Library;
+
+var result1 = MathUtilities.Add(40, 2);
+var result2 = MathUtilities.Add(0.25, 0.17);
+
+Console.WriteLine($"result1 = {result1}");
+Console.WriteLine($"result2 = {result2:G4}");
